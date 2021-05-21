2dd874a7b25980d04617760803721c2fa7ccce7b
Sergio Márquez <s.marquez@preving.com>
2021-05-21 17:12:45 +0200

    UPDATED - Ahora los métodos findByCategoriaId y findByFabricanteId retornan los resultados ordenados alfabéticamente por el nombre. Cambio en el 'encoding' del import.

--- src/main/java/com/smpcomponentes/models/dao/IComponenteDao.java
+++ src/main/java/com/smpcomponentes/models/dao/IComponenteDao.java
@@ -7,9 +7,9 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface IComponenteDao extends CrudRepository<Componente, Integer> {
-    @Query(value = "SELECT * FROM componentes c WHERE c.categoria_id = ?1", nativeQuery = true)
+    @Query(value = "SELECT * FROM componentes c WHERE c.categoria_id = ?1 ORDER BY c.nombre", nativeQuery = true)
     List<Componente> findByCategoriaId(Integer categoriaId);
 
-    @Query(value = "SELECT * FROM componentes c WHERE c.fabricante_id = ?1", nativeQuery = true)
+    @Query(value = "SELECT * FROM componentes c WHERE c.fabricante_id = ?1 ORDER BY c.nombre", nativeQuery = true)
     List<Componente> findByFabricanteId(Integer fabricanteId);
 }
