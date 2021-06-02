8e30ea9e5c31c0f7818e4b9001c3570b0d1b7520
Sergio Márquez <s.marquez@preving.com>
2021-06-03 00:34:14 +0200

    ADDED - Consulta para obtener fabricantes que aparezcan al menos en un componente.

--- src/main/java/com/smpcomponentes/models/dao/IFabricanteDao.java
+++ src/main/java/com/smpcomponentes/models/dao/IFabricanteDao.java
@@ -1,7 +1,12 @@
 package com.smpcomponentes.models.dao;
 
 import com.smpcomponentes.models.entity.Fabricante;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface IFabricanteDao extends CrudRepository<Fabricante, Integer> {
+    @Query(value = "SELECT * FROM fabricantes fab where fab.id IN (SELECT comp.fabricante_id from componentes comp)", nativeQuery = true)
+    List<Fabricante> findAllWithComponentes();
 }
