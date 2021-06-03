f99ea4e4fcf8b8f5fbc7185fcef50d77cdf5ddad
Sergio Márquez <s.marquez@preving.com>
2021-06-03 09:14:45 +0200

    UPDATED - Limit de 15 a 12 al mostrar ordenadores.

--- src/main/java/com/smpcomponentes/models/dao/IOrdenadorDao.java
+++ src/main/java/com/smpcomponentes/models/dao/IOrdenadorDao.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public interface IOrdenadorDao extends CrudRepository<Ordenador, Integer> {
 
-    @Query(value = "SELECT * FROM ordenadores o ORDER BY o.fecha_compra DESC LIMIT 15", nativeQuery = true)
+    @Query(value = "SELECT * FROM ordenadores o ORDER BY o.fecha_compra DESC LIMIT 12", nativeQuery = true)
     List<Ordenador> findAllLimit();
 
     @Query(value = "SELECT * FROM ordenadores o WHERE o.usuario_id = ?1 ORDER BY o.fecha_compra DESC LIMIT 15", nativeQuery = true)
